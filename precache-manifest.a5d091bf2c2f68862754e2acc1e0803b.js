c92a4659b81d74f072a09b86daf3e3618a06deb1
kanika <kanika@example.com>
2021-09-17 12:59:38 +0530

    Updates

---
+++ precache-manifest.a5d091bf2c2f68862754e2acc1e0803b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff3189575811c8bf2375",
+    "url": "/Contact-Manager-App/static/css/main.3c31fd40.chunk.css"
+  },
+  {
+    "revision": "ff3189575811c8bf2375",
+    "url": "/Contact-Manager-App/static/js/main.e747fe9a.chunk.js"
+  },
+  {
+    "revision": "feb1f01c378e082f8239",
+    "url": "/Contact-Manager-App/static/js/runtime~main.7d712305.js"
+  },
+  {
+    "revision": "2cd48054a52d98c3b59c",
+    "url": "/Contact-Manager-App/static/js/2.1a0a5ba6.chunk.js"
+  },
+  {
+    "revision": "f9278dda0014302ab613",
+    "url": "/Contact-Manager-App/static/js/3.bbb4e748.chunk.js"
+  },
+  {
+    "revision": "3fdee410d1707e0f23ca69d70f8c864b",
+    "url": "/Contact-Manager-App/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6521c9946f25bd0b719b9e73338fa6b2.js"
+  "/Contact-Manager-App/precache-manifest.a5d091bf2c2f68862754e2acc1e0803b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Contact-Manager-App/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
